7fa1c8aa6e81269db2c7090d2e9f1c59db94a1de
goldhorse <153879092+tosky19941209@users.noreply.github.com>
2024-09-04 18:57:52 +0900

    feat: service url is changed

--- src/service/axios.js
+++ src/service/axios.js
@@ -3,11 +3,11 @@ const port = 8080
 const api = axios.create({
     // baseURL:`https://deploy-backend-fitness-node.vercel.app/api`
     // baseURL:`https://l92.168.142.113:${port}/api`
-    // baseURL:`https://fitness-backend.vercel.app/api`
+    baseURL:`https://fitness-liard-seven.vercel.app/api`
     // baseURL:`https://77.37.86.133:8080/api`
     // baseURL:`http://146.59.157.53:${port}/api`
-    baseURL:`https://traeningsbuddy.dk/api`
-    // baseURL:`http://localhost:${port}/api`
+    //baseURL:`https://traeningsbuddy.dk/api`
+    //baseURL:`http://localhost:${port}/api`
     // baseURL:`https://backend-fitness-dk.vercel.app/api`
 })
 
